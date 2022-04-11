bbf8d51c0319af1a259f309506b5d214150bd8e3
BoiledEggs <Randomthings08491@gmail.com>
2022-04-10 22:31:30 -0400

    Pipe spawn

---
+++ Assets/PipeSpawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawn : MonoBehaviour
+{
+    public Transform Pipespawnprefab;
+    public int objectcount = 10;
+    public float spawnradius = 30;
+    public float spawnCollisionCheck;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int loop = 0; loop < objectcount; loop++)
+        {
+            Vector3 spawnPoint = transform.position + Random.insideUnitSphere * spawnradius;
+
+            if (!Physics.CheckSphere(spawnPoint, spawnCollisionCheck))
+            {
+                Instantiate(Pipespawnprefab, spawnPoint, Random.rotation);
+            }
+        }
+    }
+
+}
