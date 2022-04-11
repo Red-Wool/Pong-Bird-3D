0d6fa635da96525182f88fcd50851c388b36e9e9
RedWool <leolui0930.sia@gmail.com>
2022-04-10 20:11:12 -0400

    New Pointer Arrow
    to point toward the paddle

---
+++ Assets/Scripts/Player/PlayerEffect.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMove))]
+public class PlayerEffect : MonoBehaviour
+{
+    private Controls control;
+
+    [SerializeField] ParticleSystem flap;
+    [SerializeField] ParticleSystem glide;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(control.jump))
+        {
+            flap.Play();
+        }
+
+        if (Input.GetKeyDown(control.dash))
+        {
+            ToggleGlide(true);
+        }
+        else if (Input.GetKeyUp(control.dash))
+        {
+            ToggleGlide(false);
+        }
+    }
+
+    private void ToggleGlide(bool flag)
+    {
+        var main = glide.main;
+        main.loop = flag;
+        if (flag)
+        {
+            glide.Play();
+        }
+    }
+}
--- Assets/Scripts/Player/TrackerArrow.cs
+++ Assets/Scripts/Player/TrackerArrow.cs
@@ -10,23 +10,51 @@ public class TrackerArrow : MonoBehaviour
 
     [SerializeField] private Vector3 offset;
     [SerializeField] private float rotateSpeed;
+
+    [SerializeField] private AnimationCurve toggleSize;
+    [SerializeField] private float timeActive;
+    private float timer;
+    private bool isOn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isOn = false;
+        PaddleObject.PointScored += ActivateArrow;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         UpdateTracker();
+
+        timer = Mathf.Clamp(timer + ((isOn ? 1 : -1) * Time.deltaTime), 0f, 1f);
+        arrow.transform.localScale = Vector3.one * toggleSize.Evaluate(timer);
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            ActivateArrow();
+        }
     }
 
     public void UpdateTracker()
     {
         arrow.transform.position = follow.transform.position + offset;
         arrow.transform.rotation = Quaternion.RotateTowards(arrow.transform.rotation, Quaternion.LookRotation(target.transform.position - arrow.transform.position), rotateSpeed);
+    }
+
+    public void ActivateArrow()
+    {
+        StopCoroutine("ActivateForTime");
+        StartCoroutine(ActivateForTime(timeActive));
+    }
+
+    public IEnumerator ActivateForTime(float time)
+    {
+        isOn = true;
+        yield return new WaitForSeconds(time);
+        isOn = false;
+
     }
 
 }
